6a7b64761edbbe267e1b81095879f27964ea1678
Alek <alek.pluta@gmail.com>
2019-06-25 20:14:17 +0200

    project setup

--- app/src/main/java/com/github/alekdevninja/simpletorch/MainActivity.java
+++ app/src/main/java/com/github/alekdevninja/simpletorch/MainActivity.java
@@ -2,12 +2,27 @@ package com.github.alekdevninja.simpletorch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button buttonMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonMain = findViewById(R.id.button_main);
+        buttonMain.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_main:
+                Toast.makeText(getApplicationContext(),"click works! :)",Toast.LENGTH_SHORT).show();
+        }
     }
 }
