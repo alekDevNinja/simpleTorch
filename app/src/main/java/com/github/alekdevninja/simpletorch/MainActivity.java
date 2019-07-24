fd7ec9e50ed82329c2eee1c49ec31ca9ace04d53
Alek <alek.pluta@gmail.com>
2019-07-24 18:27:55 +0200

    cleaning repo from deleted files

--- app/src/main/java/com/github/alekdevninja/simpletorch/MainActivity.java
+++ app/src/main/java/com/github/alekdevninja/simpletorch/MainActivity.java
@@ -1,28 +1,5 @@
 package com.github.alekdevninja.simpletorch;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button buttonMain;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        buttonMain = findViewById(R.id.button_main);
-        buttonMain.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.button_main:
-                Toast.makeText(getApplicationContext(),"click works! :)",Toast.LENGTH_SHORT).show();
-        }
-    }
+public class MainActivity {
+    //clearing repo from some residuals
 }
